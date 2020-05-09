0a21f4fead484db78d419ae7487297e636522f4e
Javier Rodriguez <javirodz@gmail.com>
2020-05-08 22:26:26 -0400

    "Good"

--- broc2cisc.py
+++ broc2cisc.py
@@ -1,7 +1,41 @@
 import json
 from pprint import pprint
-#import cisco
+import re
 import yaml
+
+def load_defined_conf(zone_file="sw1-zoneshow.txt"):
+    with open(zone_file) as zone_in_text:
+        for line in zone_in_text:
+            if line.startswith("Effective"):
+                return
+            else:
+                print(line,end = '')
+    return
+
+def load_effective_conf(zone_file="sw1-zoneshow.txt"):
+    with open(zone_file) as zone_in_text:
+        for line in zone_in_text:
+            if line.startswith("Effective"):
+                print(line,end = '')
+                for line in zone_in_text:
+                    print(line,end = '')
+                return
+    return
+
+def defined_zoneset(zone_file="sw1-zoneshow.txt"):
+    defined_cfg_name = "NONE"
+    zoneset = set()
+    with open(zone_file) as zone_in_text:
+        for line in zone_in_text:
+            if line.startswith(" cfg"):
+                defined_cfg_name = line.split(':')[1]
+                for line in zone_in_text:
+                    if line.startswith(" zone"):
+                        return defined_cfg_name, zoneset
+                    else:
+                        for item in line.split(';'):
+                            zoneset.add(item)
+    return defined_cfg_name, zoneset
 
 def print_cisco_alias(vsan_id="100"):
     #print("Alias JSON")
@@ -23,6 +57,19 @@ def print_cisco_alias(vsan_id="100"):
         print("member pwwn "+wwpn)
         print("end")
 
+def cisco_zoneset(defined_cfg_name, str_zoneset, vsanid = "100", switch_id_zoneset_file="sw_zoneset"):
+    with open(switch_id_zoneset_file, 'w') as zonesetFile:
+        print("configure terminal")
+        zonesetFile.write("configure terminal\n")
+        print("zoneset name "+defined_cfg_name.replace('\t', '').replace('\n', '').replace(' ', '')+" vsan "+vsanid)
+        zonesetFile.write("zoneset name "+defined_cfg_name.replace('\t', '').replace('\n', '').replace(' ', '')+" vsan "+vsanid+"\n")
+        for i in str_zoneset.split(','):
+           if i != '':
+               print("member "+i)
+               zonesetFile.write("member "+i+"\n")
+        print("end")
+        zonesetFile.write("end\n")
+
 def main():
     #print("JSON")
     with open('broc_act_conf.json', 'r') as jsonFile:
@@ -37,6 +84,25 @@ def main():
     #    pprint (key + " : " + str(value))
 
     #print_cisco_alias("101")
+
+    #load_defined_conf()
+
+    #load_effective_conf()
+
+    #Get the defined zoneset from the zoneshowfile
+    #the zoneshow file is from the zoneshow command on a brocade switch
+    sw_zoneshow_input_file = "sw1-zoneshow.txt"
+    defined_cfg_name, zoneset = defined_zoneset(sw_zoneshow_input_file)
+    #remove spaces, newlines and tabs and leave only a csv str of the zones in the zoneset
+    str_zoneset_orig = ','.join(str(s) for s in zoneset)
+    str_zoneset_notabs = str_zoneset_orig.replace('\t', '')
+    str_zoneset_nospace = str_zoneset_notabs.replace(' ','')
+    str_zoneset = str_zoneset_nospace.replace('\n', '')
+    #print the commands to create a zoneset in a cisco mds
+    vlanid = "100"
+    switch_id_zoneset_file = "sw1_zoneset.mds"
+    cisco_zoneset(defined_cfg_name, str_zoneset, vlanid, switch_id_zoneset_file)
+
 
 if __name__ == "__main__":
     # calling the main function
